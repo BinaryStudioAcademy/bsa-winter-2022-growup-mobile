760644f4fbede7b902f02e71bff35714a488d30f
jol79 <jolhuck@gmail.com>
2022-02-07 20:01:47 +0200

    Basic structure of the redux was added with a user reducer

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
-import { Provider } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
 import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
 import { store } from '../../store/store';
 
 const LoginScreen = () => {
+  const dispatch = useAppDispatch();
+  const isAuthorized = useAppSelector(state => state.auth.isLogged);
+
   return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <Text>Login screen</Text>
-      </SafeAreaView>
-    </Provider>
+    <SafeAreaView>
+      <Text>Login screen</Text>
+    </SafeAreaView>
   );
 };
 

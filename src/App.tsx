e34bf61a5a5be878e05d942ba46466408674b47b
jol79 <jolhuck@gmail.com>
2022-02-08 20:27:38 +0200

    Redux Provider was added to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import { store } from './store/store';
 
 import RootNavigation from './navigation';
 
 const App = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <RootNavigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </StoreProvider>
   );
 };
 

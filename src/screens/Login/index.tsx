6618251cd6076dc064395a564788c4911a2b61b3
jol79 <jolhuck@gmail.com>
2022-02-08 19:55:40 +0200

    Refactoring based on the feedback in the PR #2

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,24 +3,17 @@ import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
 import { store } from '../../store/store';
 
 const LoginScreen = () => {
-  const dispatch = useAppDispatch();
-  const isLogged = useAppSelector((state) => state.user.isLogged);
-
   return (
     <Provider store={store}>
       <SafeAreaView>
-        {isLogged ? (
-            <Text>isLogged!</Text>
-        ) : (
-            <Text>not Logged!</Text>
-        )}
+        <Text>Login screen</Text>
       </SafeAreaView>
     </Provider>
   );
-};  
+};
 
 export default LoginScreen;
---
+++ src/store/user/actions.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { signIn, signOut } from './slice';
+import { ActionType } from './common';
+
+// TODO: use api call to verify user data
+const login = createAsyncThunk(
+  ActionType.SetUser,
+  async (loginPayload: any, { dispatch }) => {
+    dispatch(signIn);
+  }
+);
+
+export const authActions = {
+  signIn,
+  signOut,
+  login,
+};

9041b1ba8ef12277c6ea5acbe845346900fa09e3
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 13:41:53 +0200

    feat: text component added

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -3,4 +3,9 @@ export * from './app';
 export * from './exception';
 export * from './http';
 export * from './navigation';
+<<<<<<< HEAD
 export * from './ui';
+=======
+export * from './color';
+export * from './text-type';
+>>>>>>> d6cfc84 (feat: text component added)

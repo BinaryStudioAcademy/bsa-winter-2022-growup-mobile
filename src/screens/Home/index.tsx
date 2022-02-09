e9d959ccb5801300fa77ba2b10c0bc01d68129db
R-Mielamud <2m.roman2@gmail.com>
2022-02-09 15:44:16 +0200

    fix(#8): fix eslint command and linting errors

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type IHomeScreenProps = Record<string, any>;
 
 const HomeScreen: React.FC<IHomeScreenProps> = () => {

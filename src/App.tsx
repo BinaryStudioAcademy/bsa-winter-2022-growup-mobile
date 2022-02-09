639bd435af38f6877ff01df8585fe2eb91c4b7c1
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 17:55:43 +0200

    refactor: conflicts fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import SplashScreen from 'react-native-splash-screen';
 
-<<<<<<< HEAD
 import RootNavigation from 'src/navigation';
 import theme from 'src/styles/theme/theme';
-=======
-import RootNavigation from './navigation';
-import theme from './styles/theme/theme';
->>>>>>> 20a6df88ec11230614cf752232cfdb315bf9c680
 
 const App = () => {
   useEffect(() => {

e0a5fe77584405d3985072c92e03bc116fd043cd
jol79 <jolhuck@gmail.com>
2022-02-09 17:26:44 +0200

    Hooks now will be imported from src based on the commit with a base urls

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,9 +3,12 @@ import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { useAppDispatch } from 'src/hooks';
+import { useAppSelector } from 'src/hooks';
+
 const LoginScreen = () => {
   const dispatch = useAppDispatch();
-  const isAuthorized = useAppSelector(state => state.auth.isLogged);
+  const isAuthorized = useAppSelector(state => state.auth.isAuthorized);
 
   return (
     <SafeAreaView>
--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { authApi } from 'src/services';
 
 import { ISignInPayload } from '../../common/types';
 import { ActionType } from './common';
@@ -7,6 +8,9 @@ const signIn = createAsyncThunk(
   ActionType.SIGN_IN,
   async (payload: ISignInPayload) => {
     // TODO: implement login logic
+    const { user } = await authApi.signIn(payload);
+
+    return user;
   }
 );
 

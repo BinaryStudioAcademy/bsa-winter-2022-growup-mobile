20fd98143cbfe81d5ebb4d91516d3a98d6d78542
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 14:58:49 +0200

    feat: app theme added

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,7 @@ import { Provider as StoreProvider } from 'react-redux';
 
 import { store } from 'src/store';
 import RootNavigation from 'src/navigation';
-<<<<<<< HEAD
 import theme from 'src/styles/theme';
-=======
-import theme from 'src/styles/theme/theme';
->>>>>>> dcb3643 (feat: app theme added)
 
 const App = () => {
   useEffect(() => {
@@ -18,7 +14,6 @@ const App = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     <StoreProvider store={store}>
       <PaperProvider theme={theme}>
         <NavigationContainer>
@@ -26,13 +21,6 @@ const App = () => {
         </NavigationContainer>
       </PaperProvider>
     </StoreProvider>
-=======
-    <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <RootNavigation />
-      </NavigationContainer>
-    </PaperProvider>
->>>>>>> dcb3643 (feat: app theme added)
   );
 };
 

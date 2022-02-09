dcb36434bd231d0be544c7d4eef9144490ac9761
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 14:58:49 +0200

    feat: app theme added

---
+++ src/styles/fonts/font-config.ts
@@ -0,0 +1,24 @@
+const fontConfig = {
+  android: {
+    regular: {
+      fontFamily: 'NunitoSans-Regular',
+    },
+    light: {
+      fontFamily: 'NunitoSans-Light',
+    },
+    medium: {
+      fontFamily: 'NunitoSans-SemiBold',
+    },
+    thin: {
+      fontFamily: 'NunitoSans-Light',
+    },
+		semiBold: {
+			fontFamily: 'NunitoSans-SemiBold',
+		},
+		bold: {
+			fontFamily: 'NunitoSans-Bold',
+		}
+  },
+};
+
+export default fontConfig;

241069ec4996e269bcca15ed27db21e54a94094e
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 16:11:33 +0200

    feat: input component added

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -3,8 +3,8 @@ import { AppColors } from 'src/common/enums';
 
 const styles = StyleSheet.create({
   inputContainer: {
-		backgroundColor: AppColors.INPUT_BACKGROUND,
-	}
+    backgroundColor: AppColors.INPUT_BACKGROUND,
+  },
 });
 
 export default styles;

86348c0b65f45f120919122626fe541e557e2385
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 14:21:07 +0200

    feat: button component added

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ButtonType } from '../../common/enums';
+import Button from '../../components/button/button';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type IHomeScreenProps = Record<string, any>;
@@ -9,8 +11,20 @@ type IHomeScreenProps = Record<string, any>;
 const HomeScreen: React.FC<IHomeScreenProps> = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello there!</Text>
+      <View
+        style={{
+          margin: 30,
+          justifyContent: 'space-evenly',
+          height: 100,
+          width: 150,
+        }}
+      >
+        <Button icon='home' type={ButtonType.CONTAINED} onPress={() => {}}>
+          Button
+        </Button>
+        <Button type={ButtonType.OUTLINED} onPress={() => {}}>
+          Button2
+        </Button>
       </View>
     </SafeAreaView>
   );

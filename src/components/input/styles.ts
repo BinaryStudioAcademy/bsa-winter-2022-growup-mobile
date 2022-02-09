f2691b51282cdb06c2f02f5860e79914bfa616ed
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-09 16:11:33 +0200

    feat: input component added

---
+++ src/components/input/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { AppColors } from '../../common/enums';
+
+const styles = StyleSheet.create({
+  inputContainer: {
+		backgroundColor: AppColors.INPUT_BACKGROUND,
+	}
+});
+
+export default styles;

1e5705522a1d8984ffe0669ddc62c0e5c5c595d7
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-10 19:20:14 +0200

    GUM-27: screen basic contebt added

---
+++ src/screens/onboarding/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  content: {},
+  inputContainer: {
+    padding: 20,
+  },
+  buttonContainer: {
+    paddingLeft: 30,
+    width: 150,
+  },
+});
+
+export default styles;

d027a0d312b8afe2eab4db35b4e4e66587ed3b4c
R-Mielamud <2m.roman2@gmail.com>
2022-02-11 11:04:15 +0200

    GUM-12: fix code style

--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
+import { FAB } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Text } from 'src/components';
+import addActions from './add-actions';
+import styles from './styles';
 
 type IProfileScreenProps = Record<string, never>;
 
 const ProfileScreen: React.FC<IProfileScreenProps> = () => {
+  const [addMenuOpen, setAddMenuOpen] = useState<boolean>(false);
+
+  /*
+  const dispatch = useAppDispatch();
+
+  const addFunctions: Record<string, () => void> = {
+    skill: () => ...,
+    location: () => ...,
+    education: () => ...,
+    language: () => ...,
+    careerPoint: () => ...,
+    interest: () => ...,
+  };
+  */
+
+  const handleItemPress = (name?: string) => {
+    if (!name) {
+      return;
+    }
+
+    // addFunctions[name]();
+  };
+
+  const handleMenuStateChange = ({ open }: { open: boolean }) =>
+    setAddMenuOpen(open);
+
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.fullHeight}>
+      <View style={styles.fullHeight}>
         <Text>Profile screen!</Text>
+        <FAB.Group
+          open={addMenuOpen}
+          visible={true}
+          icon="plus"
+          actions={addActions(handleItemPress)}
+          onStateChange={handleMenuStateChange}
+        />
       </View>
     </SafeAreaView>
   );

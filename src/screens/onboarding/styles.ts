b2652b7dae93f5b54707870dfdff3926ee003a71
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-14 14:59:50 +0200

    GUM-27: user content changed

--- src/screens/onboarding/styles.ts
+++ src/screens/onboarding/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { AppColor } from 'src/common/enums';
 
 const styles = StyleSheet.create({
   content: {
     height: '90%',
     width: '100%',
+    backgroundColor: AppColor.WHITE,
   },
   buttonContainer: {
     width: '100%',

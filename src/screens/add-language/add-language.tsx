ba6f5d1e69d0838f7e10e20d905450e254405c88
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-14 20:53:39 +0200

    GUM-27: add location/language screen created

---
+++ src/screens/add-language/add-language.tsx
@@ -0,0 +1,41 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { ButtonMode, HeadingLevel } from 'src/common/enums';
+import { Heading, Input, MainButton } from 'src/components';
+import styles from './styles';
+
+type AddLanguageScreenProps = Record<string, never>;
+
+const AddLanguageScreen: React.FC<AddLanguageScreenProps> = () => {
+  const navigation = useNavigation();
+
+  const handleCancel = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <SafeAreaView>
+      <View style={styles.screen}>
+        <View>
+          <View style={styles.inputContent}>
+            <Heading style={styles.heading} level={HeadingLevel.H5}>
+              Language
+            </Heading>
+            <Input placeholder="Language" />
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <MainButton onPress={handleCancel} mode={ButtonMode.OUTLINED}>
+            Cancel
+          </MainButton>
+          <MainButton mode={ButtonMode.CONTAINED}>Add</MainButton>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default AddLanguageScreen;
--- src/screens/onboarding/components/step-content/interesting-content.tsx
+++ src/screens/onboarding/components/step-content/interesting-content.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
 import { en, registerTranslation } from 'react-native-paper-dates';
 
-import { HeadingLevel, IconName } from 'src/common/enums';
+import { HeadingLevel, IconName, OnboardingRoute } from 'src/common/enums';
 import { Heading } from 'src/components';
 import { AddButton } from '..';
 import styles from './styles';
@@ -12,6 +13,16 @@ type InterestingContentScreenProps = Record<string, never>;
 registerTranslation('en', en);
 
 const InterestingContent: React.FC<InterestingContentScreenProps> = () => {
+  const navigation = useNavigation();
+
+  const handleAddLanguage = useCallback(() => {
+    navigation.navigate(OnboardingRoute.ADD_LANGUAGE);
+  }, [navigation]);
+
+  const handleAddLocation = useCallback(() => {
+    navigation.navigate(OnboardingRoute.ADD_LOCATION);
+  }, [navigation]);
+
   return (
     <View style={styles.content}>
       <Heading style={styles.header} level={HeadingLevel.H4}>
@@ -22,6 +33,7 @@ const InterestingContent: React.FC<InterestingContentScreenProps> = () => {
           What languages do you speak?
         </Heading>
         <AddButton
+          onPress={handleAddLanguage}
           labelStyle={styles.tabButtonLabel}
           style={styles.tagButton}
           icon={IconName.CHAT_PLUS}
@@ -32,6 +44,7 @@ const InterestingContent: React.FC<InterestingContentScreenProps> = () => {
           What are your locations?
         </Heading>
         <AddButton
+          onPress={handleAddLocation}
           labelStyle={styles.tabButtonLabel}
           style={styles.tagButton}
           icon={IconName.MAP_MARKER_PLUS}

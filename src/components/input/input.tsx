ebc0aac444cd0a4c9ec60cb1060b032b34965595
jol79 <jolhuck@gmail.com>
2022-02-14 19:28:46 +0200

    UI of the login form fields was refactored based on the design

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -6,25 +6,26 @@ import { AppColor } from 'src/common/enums';
 import styles from '../shared/input-style/styles';
 import theme from '../shared/input-style/theme';
 
-type InputProps = React.ComponentPropsWithoutRef<typeof TextInput> & {
-  ref?: Ref<RNTextInput>;
-};
+type InputProps = React.ComponentPropsWithoutRef<typeof TextInput>;
 
-const Input: React.FC<InputProps> = React.forwardRef(
-  ({ label, placeholder, style, ...textInputProps }, ref) => {
-    return (
-      <TextInput
-        ref={ref}
-        style={[styles.inputContainer, style]}
-        label={label}
-        placeholder={placeholder}
-        theme={theme}
-        mode="flat"
-        activeUnderlineColor={AppColor.ACCENT}
-        {...textInputProps}
-      />
-    );
-  }
-);
+const Input: React.FC<InputProps> = ({
+  label,
+  placeholder,
+  style,
+  ...textInputProps
+}) => {
+  return (
+    <TextInput
+      style={[styles.inputContainer, style]}
+      label={label}
+      placeholder={placeholder}
+      theme={theme}
+      mode="outlined"
+      outlineColor="transparent"
+      activeUnderlineColor={AppColor.ACCENT}
+      {...textInputProps}
+    />
+  );
+};
 
 export default Input;

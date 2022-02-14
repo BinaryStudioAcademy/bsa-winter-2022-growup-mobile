46b537994613fd7920144b9fcbae73095962538a
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-02-14 14:10:19 +0200

    GUM-27: content and button styles fixed

---
+++ src/common/enums/navigation/onboarding-route.enum.ts
@@ -0,0 +1,9 @@
+enum OnboardingRoute {
+  ONBOARDING = 'Onboarding',
+  ADD_EXPERIENCE = 'Add experience',
+  ADD_EDUCATION = 'Add education',
+  ADD_LANGUAGE = 'Add language',
+  ADD_LOCATION = 'Add location',
+}
+
+export { OnboardingRoute };
---
+++ src/navigation/onboarding/index.ts
@@ -0,0 +1,1 @@
+export { default as OnboardingNavigation } from './onboarding-navigation';

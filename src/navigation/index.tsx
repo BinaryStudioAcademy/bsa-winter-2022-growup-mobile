e194104bf461b095ed80b970b9089831868595e9
R-Mielamud <2m.roman2@gmail.com>
2022-02-15 19:08:58 +0200

    GUM-11: remove create-skill-skreen from root navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { AppRoute, AuthRoute } from 'src/common/enums/navigation';
+
 import AppNavigation from './app/app-navigation';
 import { useAppSelector } from 'src/hooks';
-import { CreateSkillScreen, HomeScreen } from 'src/screens';
+import { HomeScreen } from 'src/screens';
 
 const RootStack = createNativeStackNavigator();
 
@@ -22,10 +23,7 @@ const RootNavigation = () => {
         <RootStack.Screen name={AppRoute.APP} component={AppNavigation} />
       ) : (
         <>
-          <RootStack.Screen
-            name={AuthRoute.SIGN_IN}
-            component={CreateSkillScreen}
-          />
+          <RootStack.Screen name={AuthRoute.SIGN_IN} component={HomeScreen} />
           <RootStack.Screen name={AuthRoute.SIGN_UP} component={HomeScreen} />
         </>
       )}

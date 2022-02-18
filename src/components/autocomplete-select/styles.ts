3ac27d96e0d03b698eec4d502dac61db3ae33327
R-Mielamud <2m.roman2@gmail.com>
2022-02-18 12:25:51 +0200

    GUM-29: fix

--- src/components/autocomplete-select/styles.ts
+++ src/components/autocomplete-select/styles.ts
@@ -24,8 +24,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   fill: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
   },
   touchablePosition: {
     width: '100%',

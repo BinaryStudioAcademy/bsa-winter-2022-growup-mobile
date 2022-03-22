e608af2e2971cb671e006a51b8590e728c76a994
R-Mielamud <2m.roman2@gmail.com>
2022-03-22 17:42:33 +0200

    GUM-45: replaced use selector with use app selector

--- src/screens/opportunities/opportunities.tsx
+++ src/screens/opportunities/opportunities.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect } from 'react';
 import { FlatList, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useSelector } from 'react-redux';
 
-import { RootState } from 'src/common/types';
 import { HeadingLevel } from 'src/common/enums';
 import { Heading, OpportunityCard } from 'src/components';
-import { useAppDispatch } from 'src/hooks';
+import { useAppDispatch, useAppSelector } from 'src/hooks';
 import { opportunityActions } from 'src/store/actions';
 import styles from './styles';
 
 const OpportunitiesScreen: React.FC = () => {
   const dispatch = useAppDispatch();
 
-  const { opportunitiesLoading, opportunities } = useSelector(
-    (state: RootState) => state.opportunity
+  const { opportunitiesLoading, opportunities } = useAppSelector(
+    state => state.opportunity
   );
 
   useEffect(() => {

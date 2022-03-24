416d8edbdec084e77105380cc7632975933f1f38
R-Mielamud <2m.roman2@gmail.com>
2022-03-24 12:27:24 +0200

    GUM-48: add redux

--- src/store/opportunities/actions.ts
+++ src/store/opportunities/actions.ts
@@ -1,5 +1,6 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 
+import { IFullOpportunity } from 'src/common/types';
 import { opportunitiesApi } from 'src/services';
 import { ActionTypes } from './common';
 
@@ -10,4 +11,19 @@ const loadOpportunities = createAsyncThunk(
   }
 );
 
-export { loadOpportunities };
+const loadExpandedOpportunity = createAsyncThunk<
+  IFullOpportunity | undefined,
+  string
+>(ActionTypes.LOAD_EXPANDED_OPPORTUNITY, (id: string) => {
+  return opportunitiesApi.getOpportunityById(id);
+});
+
+const unloadExpandedOpportunity = createAction(
+  ActionTypes.UNLOAD_EXPANDED_OPPORTUNITY
+);
+
+export {
+  loadOpportunities,
+  loadExpandedOpportunity,
+  unloadExpandedOpportunity,
+};
--- src/store/opportunities/common.ts
+++ src/store/opportunities/common.ts
@@ -1,5 +1,7 @@
 enum ActionTypes {
   LOAD_OPPORTUNITIES = 'opportunity/load',
+  LOAD_EXPANDED_OPPORTUNITY = 'opportunity/expanded/load',
+  UNLOAD_EXPANDED_OPPORTUNITY = 'opportunity/expanded/unload',
 }
 
 export { ActionTypes };

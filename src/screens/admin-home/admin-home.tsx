b79ddc6a68992eafa2afb236645baebb7ad7b8ff
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-03-24 15:53:06 +0200

    GUM-55: added header for admin page

--- src/screens/admin-home/admin-home.tsx
+++ src/screens/admin-home/admin-home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from 'src/components';
+import { Header } from './components';
 
 const AdminHomeScreen: React.FC = () => {
   return (
     <SafeAreaView>
+      <Header />
       <Text>Admin page</Text>
     </SafeAreaView>
   );

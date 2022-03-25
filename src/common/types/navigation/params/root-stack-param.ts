9c1976e8c15ea7e6727ec9c965e87bbe3437ba29
R-Mielamud <2m.roman2@gmail.com>
2022-03-25 15:39:40 +0200

    GUM-60: fix param type

--- src/common/types/navigation/params/root-stack-param.ts
+++ src/common/types/navigation/params/root-stack-param.ts
@@ -7,7 +7,9 @@ type RootStackParamList = {
   [AppRoute.APP]: NavigatorScreenParams<AppStackParamList>;
   [AuthRoute.SIGN_IN]: undefined;
   [AuthRoute.SIGN_UP]: undefined;
-  [AuthRoute.COMPLETE_REGISTRATION]: undefined;
+  [AuthRoute.COMPLETE_REGISTRATION]: {
+    token: string;
+  };
 };
 
 export type { RootStackParamList };

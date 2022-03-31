ef9f476c28f15afd5defe3f49d71b6a2b1d62b1a
Kateryna Kostikova <katyakostikova21@gmail.com>
2022-03-31 14:47:06 +0300

    GUM-74: education features added

--- src/common/types/navigation/params/onboarding-stack-param.ts
+++ src/common/types/navigation/params/onboarding-stack-param.ts
@@ -1,9 +1,12 @@
 import { OnboardingRoute } from 'src/common/enums';
 import { ICareer } from '../../career';
+import { IEducation } from '../../education';
 
 type OnboardingStackParamList = {
   [OnboardingRoute.ONBOARDING]: undefined;
-  [OnboardingRoute.ADD_EDUCATION]: undefined;
+  [OnboardingRoute.ADD_EDUCATION]: {
+    _education?: IEducation;
+  };
   [OnboardingRoute.ADD_EXPERIENCE]: {
     career?: ICareer;
   };
--- src/screens/onboarding/components/step-content/education-content.tsx
+++ src/screens/onboarding/components/step-content/education-content.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ScrollView, View } from 'react-native';
 
 import { HeadingLevel, OnboardingRoute } from 'src/common/enums';
-import { Heading, AddButton } from 'src/components';
-import { useAppNavigation } from 'src/hooks';
+import { IEducation } from 'src/common/types';
+import { Heading, AddButton, EducationCard } from 'src/components';
+import { useAppDispatch, useAppNavigation, useAppSelector } from 'src/hooks';
+import { educationActions } from 'src/store/education';
 import useStyles from './styles';
 
 const EducationContent: React.FC = () => {
   const styles = useStyles();
-
   const navigation = useAppNavigation();
+  const dispatch = useAppDispatch();
+
+  const { education } = useAppSelector(state => state.education);
 
   const handleAddEducation = () => {
-    navigation.navigate(OnboardingRoute.ADD_EDUCATION);
+    navigation.navigate(OnboardingRoute.ADD_EDUCATION, {
+      _education: undefined,
+    });
   };
+
+  const handleDeleteEducation = useCallback(
+    (id: string) => {
+      dispatch(educationActions.deleteEducationExperience(id));
+    },
+    [dispatch]
+  );
+
+  const handleEditEducation = useCallback(
+    (_education: IEducation) => {
+      navigation.navigate(OnboardingRoute.ADD_EDUCATION, {
+        _education,
+      });
+    },
+    [navigation]
+  );
 
   return (
     <View style={styles.content}>
@@ -24,7 +46,15 @@ const EducationContent: React.FC = () => {
         <AddButton onPress={handleAddEducation}>Add Education</AddButton>
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
-        {/* TODO map education card */}
+        {education.map(educ => (
+          <EducationCard
+            style={styles.cardItem}
+            key={educ.id}
+            education={educ}
+            onEdit={handleEditEducation}
+            onDelete={handleDeleteEducation}
+          />
+        ))}
       </ScrollView>
     </View>
   );

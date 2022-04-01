394a20144cd90aec0c7aabe72a9815138cdf46f4
R-Mielamud <2m.roman2@gmail.com>
2022-04-01 20:31:08 +0300

    GUM-85: add redux

--- src/store/okr/common.ts
+++ src/store/okr/common.ts
@@ -1,4 +1,5 @@
 enum ActionTypes {
+  LOAD_OKR = 'okr/load',
   CREATE_OKR = 'okr/create',
   ADD_KEY_RESULT = 'okr/add-key-result',
 }
--- src/store/okr/slice.ts
+++ src/store/okr/slice.ts
@@ -1,14 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { IOkr } from 'src/common/types';
 
 import { IAddOkr } from 'src/common/types/okr/add-okr.interface';
-import { createOKR, addKeyResult } from './actions';
+import { createOKR, addKeyResult, loadOKRs } from './actions';
 
 interface IOKRState {
-  current: IAddOkr; // create OKR key results
-  objectives: IAddOkr[]; // TODO change to IOKR
+  okrs: IOkr[];
+  okrsLoading: boolean;
+  current: IAddOkr;
+  objectives: IAddOkr[];
 }
 
 const initialState: IOKRState = {
+  okrs: [],
+  okrsLoading: false,
   current: { name: '', cycle: '', parent: '', teamName: '', keyResults: [] },
   objectives: [],
 };
@@ -18,6 +23,10 @@ const slice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
+    builder.addCase(loadOKRs.fulfilled, (state, { payload }) => {
+      state.okrs = payload;
+    });
+
     builder.addCase(createOKR.fulfilled, (state, { payload }) => {
       if (!payload) {
         return;
@@ -30,9 +39,26 @@ const slice = createSlice({
       state.current.cycle = '';
       state.current.teamName = '';
     });
+
     builder.addCase(addKeyResult, (state, { payload }) => {
       state.current.keyResults.push(payload);
     });
+
+    builder.addMatcher(isAnyOf(loadOKRs.pending, createOKR.pending), state => {
+      state.okrsLoading = true;
+    });
+
+    builder.addMatcher(
+      isAnyOf(
+        loadOKRs.fulfilled,
+        createOKR.fulfilled,
+        loadOKRs.rejected,
+        createOKR.rejected
+      ),
+      state => {
+        state.okrsLoading = false;
+      }
+    );
   },
 });
 
